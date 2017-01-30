35ace102ae9320d8a08bc8e70112911bb41e7493
kuldeep-izap <kuldeep.spall@izap.in>
2017-01-30 19:34:40 +0530

    mobile app learing

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: '<h1>{{title}}</h1><h2>{{hero}} details!</h2>',
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent {
+  title = 'Tour of Heroes';
+  hero = 'Windstorm';
+}
+
+export class Hero {
+  id: number;
+  name: string;
+}
